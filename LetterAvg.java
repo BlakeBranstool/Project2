0e8f36d882a955af1a81928e01920e113f5b6c75
Blake Branstool <blake.branstool@ou.edu>
2019-10-08 23:53:13 -0500

    Skeleton for letterAvg

---
+++ LetterAvg.java
@@ -0,0 +1,21 @@
+
+public class LetterAvg extends MesoInherit {
+
+	public LetterAvg(MesoStation mesoStation) {
+		super(mesoStation);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	int[] calAverage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	char letterAverage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
