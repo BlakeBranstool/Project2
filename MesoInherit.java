9cefb3d849d790e43e4e8d61563de23fa037c675
Blake Branstool <blake.branstool@ou.edu>
2019-10-08 22:22:42 -0500

    base class for MesoInherit

---
+++ MesoInherit.java
@@ -0,0 +1,18 @@
+
+public class MesoInherit {
+	
+	private MesoStation mesoStation;
+	
+	public MesoInherit(MesoStation mesoStation) {
+		setMesoStation(mesoStation);
+	}
+
+	public MesoStation getMesoStation() {
+		return mesoStation;
+	}
+
+	public void setMesoStation(MesoStation mesoStation) {
+		this.mesoStation = mesoStation;
+	}
+	
+}
