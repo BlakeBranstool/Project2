d33cd5d8d38f628b2be5af9736d696c3c921176b
Blake Branstool <blake.branstool@ou.edu>
2019-10-08 22:52:01 -0500

    Base class PosAvg

---
+++ PosAvg.java
@@ -0,0 +1,18 @@
+
+public class PosAvg {
+
+	private String stid;
+	
+	public PosAvg(String stid) {
+		setStid(stid);
+	}
+	
+	public String getStid() {
+		return stid;
+	}
+
+	public void setStid(String stid) {
+		this.stid = stid;
+	}
+	
+}
